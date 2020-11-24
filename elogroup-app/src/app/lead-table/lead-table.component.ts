188e0e55b0fac06f3585ac43f09b26552efa2de4
Guilherme Melo <guilhermearmelo@gmail.com>
2020-11-24 09:51:57 -0300

    Fix Duplicated Lead on Succesfull Drop

--- elogroup-app/src/app/lead-table/lead-table.component.ts
+++ elogroup-app/src/app/lead-table/lead-table.component.ts
@@ -88,7 +88,7 @@ export class LeadTableComponent implements OnInit {
                 x.status++;
               }
             }
-            this.selectLeads();
+            //this.selectLeads();
           });
       }
 
